84891839c5be806cc94c55bd425edef78ff53f30
Sanjay S S <109343725+Sanjayss24@users.noreply.github.com>
2022-09-04 19:41:46 +0530

    Update foodorder.service.ts

--- src/app/Services/foodorder.service.ts
+++ src/app/Services/foodorder.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FoodorderService {
+export class FoodOrderService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  saveFoodOrder(foodOrder:any){
+    return this.http.post(`http://localhost:8080/api/savefo`,foodOrder)
+  }
+
+  getFoodOrder(){
+    return this.http.get('http://localhost:8080/api/allfo');
+  }
+
+  updateFoodOrder(id:any,foodOrder:any){
+   return this.http.put(`http://localhost:8080/api/updatefo/${id}`,foodOrder)
+  }
+
+  deletefo(id:any){
+    return this.http.delete(`http://localhost:8080/api/deletefo/${id}`);
+  }
 }

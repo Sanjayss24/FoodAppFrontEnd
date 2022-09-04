99a86ee93c7d9c578dbc7877af94b271e5f975f5
Sanjay S S <109343725+Sanjayss24@users.noreply.github.com>
2022-09-04 20:07:34 +0530

    Update edit-food-order.component.ts

--- src/app/edit-food-order/edit-food-order.component.ts
+++ src/app/edit-food-order/edit-food-order.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { FoodOrder } from '../food-order';
+import { FoodOrderService } from '../Services/food-order.service';
 
 @Component({
   selector: 'app-edit-food-order',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-food-order.component.css']
 })
 export class EditFoodOrderComponent implements OnInit {
-
-  constructor() { }
+  result:any;
+  constructor(private food:FoodOrderService, private route:ActivatedRoute) { }
+   foodOrder=new FoodOrder();
 
   ngOnInit(): void {
+     this.food.getFoodOrder().subscribe((Data)=>{
+      console.log(Data);
+      this.result=Data;
+
+      let id = this.route.snapshot.params['id'];
+
+      for(let r of this.result){
+        if(id==r.id){
+          this.foodOrder=r;
+          console.log(this.foodOrder);
+          
+        }
+      }
+      
+     })  
+
+  }
+
+  updatefo(form:NgForm){
+    this.food.updateFoodOrder(this.foodOrder.id,this.foodOrder).subscribe((Data)=>{
+      console.log(Data);
+      
+    })
   }
 
 }

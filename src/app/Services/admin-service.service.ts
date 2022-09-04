68e4f096529946b0bb50b412ab659b466ca23f50
Sanjay S S <109343725+Sanjayss24@users.noreply.github.com>
2022-09-04 19:43:17 +0530

    Update admin-service.service.ts

--- src/app/Services/admin-service.service.ts
+++ src/app/Services/admin-service.service.ts
@@ -12,9 +12,16 @@ export class AdminServiceService {
     return this.http.get(`http://localhost:8080/api/alladmin`);
   }
   
-
   addAdmins(admin:any){
     return this.http.post(`http://localhost:8080/api/saveadmin`,admin);
+  }
+
+  updateAdmin(id:any,admin:any){
+   return this.http.put(`http://localhost:8080/api/updateadmin/${id}`,admin)
+  }
+
+  deleteAdmin(id:any){
+    return this.http.delete(`http://localhost:8080/api/deleteadmin/${id}`);
   }
 
 }

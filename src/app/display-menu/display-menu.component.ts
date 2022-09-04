a0bf0ee42a9642839789682778031957be2c0606
Sanjay S S <109343725+Sanjayss24@users.noreply.github.com>
2022-09-04 19:58:44 +0530

    Update display-menu.component.ts

--- src/app/display-menu/display-menu.component.ts
+++ src/app/display-menu/display-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../Services/menu.service';
 
 @Component({
   selector: 'app-display-menu',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-menu.component.css']
 })
 export class DisplayMenuComponent implements OnInit {
-
-  constructor() { }
+   marray:any;
+  constructor(private menu:MenuService) { }
 
   ngOnInit(): void {
+    this.menu.getMenu().subscribe((Data)=>{
+      console.log(Data);
+      this.marray=Data;
+      
+    })
   }
 
 }

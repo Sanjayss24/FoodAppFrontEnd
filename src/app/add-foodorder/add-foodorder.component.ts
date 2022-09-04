d4a686018e402ede9add57ba43b09febca4e8b75
Sanjay S S <109343725+Sanjayss24@users.noreply.github.com>
2022-09-04 19:46:14 +0530

    Update add-foodorder.component.ts

--- src/app/add-foodorder/add-foodorder.component.ts
+++ src/app/add-foodorder/add-foodorder.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FoodOrder } from '../food-order';
+import { FoodOrderService } from '../Services/food-order.service';
 
 @Component({
   selector: 'app-add-foodorder',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFoodorderComponent implements OnInit {
 
-  constructor() { }
+ 
+
+  constructor(private foodOrderService:FoodOrderService) { }
+  
+  foodOrder = new FoodOrder();
+  
 
   ngOnInit(): void {
   }
+   addFoodOrder(form:NgForm){
+    console.log(form.value);
+    console.log(this.foodOrder);
+    
 
+    this.foodOrderService.saveFoodOrder(this.foodOrder).subscribe((res)=>{
+      console.log(res);
+      
+    })
+
+   }
 }

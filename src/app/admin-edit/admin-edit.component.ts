776d70db5c23bebbf70beb7bb76d9fc6fc128142
Sanjay S S <109343725+Sanjayss24@users.noreply.github.com>
2022-09-04 19:51:30 +0530

    Update admin-edit.component.ts

--- src/app/admin-edit/admin-edit.component.ts
+++ src/app/admin-edit/admin-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Admin } from '../admin';
+import { ActivatedRoute } from '@angular/router';
+import { AdminServiceService } from '../Services/admin-service.service';
 
 @Component({
   selector: 'app-admin-edit',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-edit.component.css']
 })
 export class AdminEditComponent implements OnInit {
-
-  constructor() { }
-
+  result:any;
+  constructor(private adminup:AdminServiceService,private route:ActivatedRoute) { }
+  admin=new Admin();
   ngOnInit(): void {
+
+    this.adminup.getAdmin().subscribe((data)=>{
+      this.result=data;
+      console.log(this.result);
+
+      let id= this.route.snapshot.params['id'];
+
+
+      for(let r of this.result.t){
+        if (id==r.id){
+          this.admin=r;
+          console.log(this.admin);
+        }
+        }
+      })
+  }
+
+  updateAdmin(form:NgForm){
+    this.adminup.updateAdmin(this.admin.id,this.admin).subscribe((Data)=>{
+      console.log(Data);
+    })
   }
 
 }

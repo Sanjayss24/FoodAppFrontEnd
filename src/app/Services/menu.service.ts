e9aa8586d5ba25e17ee89e16ee56be88c5d3b99f
Sanjay S S <109343725+Sanjayss24@users.noreply.github.com>
2022-09-04 19:39:58 +0530

    Update menu.service.ts

--- src/app/Services/menu.service.ts
+++ src/app/Services/menu.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class MenuService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getMenu(){
+    return this.http.get(`http://localhost:8080/api/menu`);
+  }
 }

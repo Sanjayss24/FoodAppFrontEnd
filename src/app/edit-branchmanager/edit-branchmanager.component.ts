5fb9ccd2ff51617924e76000b3c5387770c290cf
Sanjay S S <109343725+Sanjayss24@users.noreply.github.com>
2022-09-04 20:32:37 +0530

    Update edit-branchmanager.component.ts

--- src/app/edit-branchmanager/edit-branchmanager.component.ts
+++ src/app/edit-branchmanager/edit-branchmanager.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { BranchManager } from '../branch-manager';
+import { BranchmanagerService } from '../Services/branchmanager.service';
 
 @Component({
   selector: 'app-edit-branchmanager',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-branchmanager.component.css']
 })
 export class EditBranchmanagerComponent implements OnInit {
-
-  constructor() { }
-
+  result:any;
+  constructor(private bmservice:BranchmanagerService, private route:ActivatedRoute) { }
+ branchManager=new BranchManager();
   ngOnInit(): void {
-  }
+    this.bmservice.getBranchManager().subscribe((data)=>{
+      this.result=data;
+      console.log(this.result);
+
+      let id= this.route.snapshot.params['id'];
+
+
+      for(let r of this.result.t){
+        if (id==r.id){
+          this.branchManager=r;
+          console.log(this.branchManager);
+        }
+        }
+      })
+    
+ }
+ updatebm(form:NgForm){
+  this.bmservice.updateBranchManager(this.branchManager.id,this.branchManager).subscribe((Data)=>{
+    console.log(Data);
+  })
+}
+
+
 
 }
